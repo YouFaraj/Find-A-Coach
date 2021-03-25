e97058202fbdf93831bebc9bc3889830709b1219
Yousif Faraj <yfaraj96@gmail.com>
2021-03-25 19:35:51 -0400

    uploading data works

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,24 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootGetters.userId;
     const coachData = {
-      id: context.rootGetters.userId,
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas
     }
-    context.commit('registerCoach', coachData);
+    const response = await fetch(`https://findacoach-e2305-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    })
+    if(!response.ok) {
+      console.log('Whoops')
+    }
+
+    context.commit('registerCoach', {
+      ...coachData,
+      id: userId
+    });
   }
 }
