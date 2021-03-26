5c7ed48246635577d57c6c2ad261b37146b08a74
Yousif Faraj <yfaraj96@gmail.com>
2021-03-26 00:09:39 -0400

    error with obj placement requests

--- src/pages/requests/RequestsRecieved.vue
+++ src/pages/requests/RequestsRecieved.vue
@@ -1,16 +1,21 @@
 <template>
+  <base-dialog :show="!!error" title="An error occured!" @close="handleError">
+    <p>{{ error }}</p>
+  </base-dialog>
   <section>
     <base-card>
       <header>
         <h2>Requests Recieved</h2>
       </header>
-      <ul v-if="hasRequests">
+      <base-spinner v-if="isLoading"></base-spinner>
+      <ul v-else-if="hasRequests && !this.isLoading">
         <request-item
           v-for="req in receivedRequests"
           :key="req.id"
           :email="req.userEmail"
           :message="req.message"
-        ></request-item>
+        >
+        </request-item>
       </ul>
       <h3 v-else>You haven't recieved any requests yet</h3>
     </base-card>
@@ -20,6 +25,12 @@
 <script>
 import RequestItem from '../../components/requests/RequestItem.vue';
 export default {
+  data() {
+    return {
+      isLoading: false,
+      error: null
+    };
+  },
   components: {
     RequestItem
   },
@@ -29,6 +40,23 @@ export default {
     },
     hasRequests() {
       return this.$store.getters['requests/hasRequests'];
+    }
+  },
+  created() {
+    this.loadRequests()
+  },
+  methods: {
+    async loadRequests() {
+      this.isLoading = true;
+      try {
+        await this.$store.dispatch('requests/fetchRequests');
+      } catch (error) {
+        this.error = error.message || 'Something Failed!';
+      }
+      this.isLoading = false;
+    },
+    handleError() {
+      this.error = null;
     }
   }
 };
--- src/store/modules/requests/mutations.js
+++ src/store/modules/requests/mutations.js
@@ -1,5 +1,8 @@
 export default {
   addRequest(state, payload) {
-    return state.requests.push(payload)
+    state.requests.push(payload)
+  },
+  setRequests(state, payload) {
+    state.requests = payload
   }
 }
