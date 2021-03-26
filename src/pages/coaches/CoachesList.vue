2acd42459d6e29fb34888c46b8de88fb30d4ad62
Yousif Faraj <yfaraj96@gmail.com>
2021-03-25 20:22:10 -0400

    pull data successful

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -5,7 +5,7 @@
   <section>
     <base-card>
       <div class="controls">
-        <base-button mode="outline">
+        <base-button mode="outline" @click="loadCoaches">
           Refresh
         </base-button>
         <base-button v-if="!isCoach" link to="/register">
@@ -71,7 +71,13 @@ export default {
   methods: {
     setFilters(updatedFilters) {
       this.activeFilters = updatedFilters;
+    },
+    loadCoaches() {
+      this.$store.dispatch('coaches/loadCoaches');
     }
+  },
+  created() {
+    this.loadCoaches();
   }
 };
 </script>
--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -20,5 +20,25 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context) {
+    const response = await fetch('https://findacoach-e2305-default-rtdb.firebaseio.com/coaches.json')
+    const responseData = await response.json();
+    if(!response.ok) {
+      console.log('Whoops from loadCoaches')
+    }
+    const coaches = [];
+    for(const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas,
+      }
+      coaches.push(coach);
+    }
+    context.commit('setCoaches', coaches)
   }
 }
